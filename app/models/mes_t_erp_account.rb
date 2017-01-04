abbe440c44efcf86c60d683750eca2ff66e41ddf
lum <ibmt20@hotmail.com>
2017-01-04 20:58:05 +0800

    Changes

--- app/models/mes_t_erp_account.rb
+++ app/models/mes_t_erp_account.rb
@@ -18,14 +18,137 @@ class MesTErpAccount < ActiveRecord::Base
   java_import 'com.sap.conn.jco.ext.DestinationDataProvider'
   java_import 'com.sap.conn.jco.JCoContext'
 
-  def self.sap_posting_261
-    sql = "select order_id from t_erp_account where status='10' and quantity > 0 group by order_id"
+  def self.start
+    adjustment
+    mo_return
+    mo_issue
+  end
+
+  def self.adjustment
+    MesTErpAccount.transaction do
+      stk_returns = MesTErpAccount.where("status='10' and move_type='262' and quantity < 0").order(:order_id)
+      stk_returns.each do |stk_return|
+        stk_return.ws_bal_qty = stk_return.quantity - stk_return.sap_posted_qty - stk_return.mes_inter_qty
+        stk_return.ws_alloc_qty = 0
+
+        if stk_return.ws_bal_qty < 0
+          stk_issues = MesTErpAccount.where("status='10' and move_type='261' and quantity > 0")
+                           .where(material: stk_return.material, order_id: stk_return.order_id, bacth: stk_return.bacth)
+                           .order(:order_id)
+          stk_issues.each do |stk_issue|
+            stk_issue.ws_bal_qty = stk_issue.quantity - stk_issue.sap_posted_qty - stk_return.mes_inter_qty
+            stk_issue.ws_alloc_qty = 0
+            if stk_issue.ws_bal_qty > 0
+              ws_qty = stk_return.ws_bal_qty.abs > stk_issue.ws_bal_qty.abs ? stk_issue.ws_bal_qty.abs : stk_return.ws_bal_qty.abs
+              stk_return.ws_alloc_qty -= ws_qty
+              stk_return.ws_bal_qty += ws_qty
+              stk_return.mes_inter_qty -= ws_qty
+              stk_issue.ws_alloc_qty += ws_qty
+              stk_issue.ws_bal_qty -= ws_qty
+              stk_issue.mes_inter_qty += ws_qty
+              MesTErpAccountIss.create(
+                  uuid: UUID.new.generate(:compact),
+                  tea_id: stk_return.id,
+                  mblnr: 0,
+                  mjahr: Time.now.strftime('%Y'),
+                  zeile: 0,
+                  matnr: stk_return.material,
+                  charg: stk_return.bacth,
+                  menge: ws_qty * -1,
+                  werks: stk_return.plant,
+                  lgort: 'MES',
+                  rsnum: ' ',
+                  rspos: 0,
+                  vtweg: PoReceipt.vtweg(stk_return.plant)
+              )
+              MesTErpAccountIss.create(
+                  uuid: UUID.new.generate(:compact),
+                  tea_id: stk_issue.id,
+                  mblnr: 0,
+                  mjahr: Time.now.strftime('%Y'),
+                  zeile: 0,
+                  matnr: stk_issue.material,
+                  charg: stk_issue.bacth,
+                  menge: ws_qty,
+                  werks: stk_issue.plant,
+                  lgort: 'MES',
+                  rsnum: ' ',
+                  rspos: 0,
+                  vtweg: PoReceipt.vtweg(stk_issue.plant)
+              )
+              stk_issue.status = 'X' if stk_issue.ws_bal_qty == 0
+              stk_issue.save
+
+              stk_return.status = 'X' if stk_return.ws_bal_qty == 0
+              stk_return.save
+            end
+            break if stk_return.ws_bal_qty == 0
+          end
+        end
+      end
+    end
+  end
+
+  def self.mo_return
+    stk_issues_hash = {}
+    stk_returns = MesTErpAccount.where("status='10' and move_type='262' and quantity < 0").order(:order_id)
+    stk_returns.each do |stk_return|
+      stk_return.ws_bal_qty = stk_return.quantity - stk_return.sap_posted_qty - stk_return.mes_inter_qty
+      stk_return.ws_alloc_qty = 0
+      stk_return.ws_movements = []
+      if stk_return.ws_bal_qty < 0
+        #find posted transactions
+        key = "#{stk_return.order_id}.#{stk_return.material}.#{stk_return.bacth}.#{stk_return.plant}"
+        if not stk_issues_hash.key?(key)
+          sql = "
+            select b.rsnum,b.rspos,b.werks,b.posnr,sum(b.menge) menge from t_erp_account a
+              join t_erp_account_iss b on b.tea_id=a.id and b.mblnr <> '0' and b.aufnr='#{stk_return.order_id}'
+            where a.material='#{stk_return.material}' and a.bacth='#{stk_return.bacth}' and a.plant='#{stk_return.plant}'
+            group by b.rsnum,b.rspos,b.werks,b.posnr
+            having sum(menge) > 0
+            order by b.posnr desc
+         "
+          stk_issues = MesTErpAccount.find_by_sql(sql)
+          stk_issues_hash[key] = stk_issues
+        end
+        stk_issues = stk_issues_hash[key]
+        stk_issues.each do |stk_issue|
+          ws_qty = stk_return.ws_bal_qty.abs > stk_issue.menge.abs ? stk_issue.menge.abs : stk_return.ws_bal_qty.abs
+          stk_return.ws_alloc_qty -= ws_qty
+          stk_return.ws_bal_qty += ws_qty
+          stk_issue.menge -= ws_qty
+          if ws_qty > 0
+            stk_return.ws_movements.append({
+                                               rsnum: stk_issue.rsnum, rspos: stk_issue.rspos, rsart: ' ',
+                                               lgort: 'MES', menge: ws_qty, aufnr: stk_return.order_id, posnr: stk_issue.posnr,
+                                               move_type: '262'
+                                           })
+
+
+          end
+          break if stk_return.ws_bal_qty == 0
+        end
+      end
+    end
+    bapi_goodsmvt_create_261(stk_returns)
+    # stk_returns.each do |account|
+    #   account.ws_movements.each do |hash|
+    #     puts hash
+    #   end
+    # end
+  end
+
+
+  def self.mo_issue
+    sql = "select order_id from t_erp_account where status='10' and quantity > 0 and move_type='261' group by order_id"
     MesTErpAccount.find_by_sql(sql).each do |order|
       mat_lot_refs = []
-      mes_t_erp_accounts = MesTErpAccount.where(status: '10', order_id: order.order_id).where("quantity > 0 and move_type='261' and ((sysdate - updated_time)*24*60) > 15")
+      mes_t_erp_accounts = MesTErpAccount
+                               .where(status: '10', order_id: order.order_id).where("quantity > 0 and move_type='261' and ((sysdate - updated_time)*24*60) > 15")
+                               .order(id: :asc)
       mes_t_erp_accounts.each do |row|
         MesTErpAccount.connection.execute("update t_erp_account set updated_time = sysdate where id=#{row.id}")
-        row.ws_bal_qty = row.quantity - row.sap_posted_qty
+        row.ws_bal_qty = row.quantity - row.sap_posted_qty - row.mes_inter_qty
         row.ws_alloc_qty = 0
         row.ws_locations = []
         row.ws_movements = []
@@ -47,146 +170,49 @@ class MesTErpAccount < ActiveRecord::Base
           mchbs[key].append(row)
         end
       end
-
-      sql = "
+      orders = order.order_id.split(',')
+      orders.each do |aufnr|
+        sql = "
         select a.rsnum, a.rspos, a.matnr, a.werks, a.lgort, a.rsart,
                a.bdmng, a.enmng, (a.bdmng - a.enmng) bal_qty,
-               a.posnr, d.arbpl
+               a.posnr, d.arbpl, a.aufnr
           from sapsr3.resb a
                  join sapsr3.afvc  c on c.mandt=a.mandt and c.aufpl=a.aufpl and c.aplzl=a.aplzl and c.plnfl=a.plnfl
             left join sapsr3.crhd  d on d.mandt=c.mandt and d.objty='A' and d.objid=c.arbid and a.bdter between d.begda and d.endda
           where a.mandt='168' and a.dumps=' ' and a.bdmng <> 0 and a.xloek=' ' and a.aufnr=?
       "
-      resbs = Sapdb.find_by_sql([sql, order.order_id])
-      resbs.each do |resb|
-        if resb.bal_qty > 0
-          mes_t_erp_accounts.each do |row|
-            if row.ws_bal_qty > 0 and row.material.eql?(resb.matnr) and row.work_center.eql?(resb.arbpl)
-              key = "#{row.plant}.#{row.material}.#{row.bacth}"
-              if mchbs.key?(key)
-                mchbs[key].each do |mchb|
-                  if mchb.clabs > 0 and mchb.lgort.eql?('MES')
-                    mchb_qty = row.ws_bal_qty > mchb.clabs ? mchb.clabs : row.ws_bal_qty
-                    resb_qty = resb.bal_qty > mchb_qty ? mchb_qty : resb.bal_qty
-                    row.ws_bal_qty -= resb_qty
-                    row.ws_alloc_qty += resb_qty
-                    mchb.clabs -= resb_qty
-                    resb.bal_qty -= resb_qty
-                    if resb_qty > 0
-                      row.ws_movements.append({
-                                                  rsnum: resb.rsnum, rspos: resb.rspos, rsart: resb.rsart,
-                                                  lgort: mchb.lgort, menge: resb_qty
-                                              })
+        resbs = Sapdb.find_by_sql([sql, aufnr])
+        resbs.each do |resb|
+          if resb.bal_qty > 0
+            mes_t_erp_accounts.each do |row|
+              if row.ws_bal_qty > 0 and row.material.eql?(resb.matnr) and row.work_center.eql?(resb.arbpl)
+                key = "#{row.plant}.#{row.material}.#{row.bacth}"
+                if mchbs.key?(key)
+                  mchbs[key].each do |mchb|
+                    if mchb.clabs > 0 and mchb.lgort.eql?('MES')
+                      mchb_qty = row.ws_bal_qty > mchb.clabs ? mchb.clabs : row.ws_bal_qty
+                      resb_qty = resb.bal_qty > mchb_qty ? mchb_qty : resb.bal_qty
+                      row.ws_bal_qty -= resb_qty
+                      row.ws_alloc_qty += resb_qty
+                      mchb.clabs -= resb_qty
+                      resb.bal_qty -= resb_qty
+                      if resb_qty > 0
+                        row.ws_movements.append({
+                                                    rsnum: resb.rsnum, rspos: resb.rspos, rsart: resb.rsart,
+                                                    lgort: mchb.lgort, menge: resb_qty, aufnr: aufnr, posnr: resb.posnr
+                                                })
+                      end
                     end
-                  end
-                  break if row.ws_bal_qty == 0
-                end #mchbs[key].each do |mchb|
+                    break if row.ws_bal_qty == 0
+                  end #mchbs[key].each do |mchb|
+                end
               end
-            end
-          end # mes_t_erp_accounts.each do |row|
-        end #if resb.bal_qty > 0
-
-        # if resb.bal_qty > 0
-        #   mes_t_erp_accounts.each do |row|
-        #     if row.ws_bal_qty > 0 and row.material.eql?(resb.matnr) and row.work_center.eql?(resb.arbpl)
-        #       key = "#{row.plant}.#{row.material}.#{row.bacth}"
-        #       if mchbs.key?(key)
-        #         mchbs[key].each do |mchb|
-        #           if mchb.clabs > 0 and mchb.lgort.eql?(resb.lgort)
-        #             mchb_qty = row.ws_bal_qty > mchb.clabs ? mchb.clabs : row.ws_bal_qty
-        #             resb_qty = resb.bal_qty > mchb_qty ? mchb_qty : resb.bal_qty
-        #             row.ws_bal_qty -= resb_qty
-        #             row.ws_alloc_qty += resb_qty
-        #             mchb.clabs -= resb_qty
-        #             resb.bal_qty -= resb_qty
-        #             row.ws_movements.append({
-        #                                         rsnum: resb.rsnum, rspos: resb.rspos, rsart: resb.rsart,
-        #                                         lgort: mchb.lgort, menge: resb_qty
-        #                                     })
-        #           end
-        #           break if row.ws_bal_qty == 0
-        #         end #mchbs[key].each do |mchb|
-        #       end
-        #     end
-        #     break if resb.bal_qty = 0
-        #   end # mes_t_erp_accounts.each do |row|
-        # end #if resb.bal_qty > 0
-
-        #   #process 2 - over issue not allowed, diff location allow
-        #   if resb.bal_qty > 0
-        #     mes_t_erp_accounts.each do |row|
-        #       if row.ws_bal_qty > 0 and row.material.eql?(resb.matnr) and row.work_center.eql?(resb.arbpl)
-        #         key = "#{row.plant}.#{row.material}.#{row.bacth}"
-        #         if mchbs.key?(key)
-        #           mchbs[key].each do |mchb|
-        #             if mchb.clabs > 0
-        #               mchb_qty = row.ws_bal_qty > mchb.clabs ? mchb.clabs : row.ws_bal_qty
-        #               resb_qty = resb.bal_qty > mchb_qty ? mchb_qty : resb.bal_qty
-        #               row.ws_bal_qty -= resb_qty
-        #               row.ws_alloc_qty += resb_qty
-        #               mchb.clabs -= resb_qty
-        #               resb.bal_qty -= resb_qty
-        #               row.ws_movements.append({
-        #                                           rsnum: resb.rsnum, rspos: resb.rspos, rsart: resb.rsart,
-        #                                           lgort: mchb.lgort, menge: resb_qty
-        #                                       })
-        #             end
-        #             break if row.ws_bal_qty == 0
-        #           end #mchbs[key].each do |mchb|
-        #         end
-        #       end
-        #       break if resb.bal_qty = 0
-        #     end # mes_t_erp_accounts.each do |row|
-        #   end #if resb.bal_qty > 0
-      end # resbs.each do |resb|
-
-      # mes_t_erp_accounts.each do |row|
-      #   if row.ws_bal_qty > 0
-      #     rows = resbs.select { |a| a.matnr.eql?(row.material) and a.werks.eql?(row.plant) and
-      #         a.arbpl.eql?(row.work_center) and a.posnr.eql?('Z002') }
-      #
-      #     if rows.present?
-      #       resb = rows.first
-      #     else
-      #       rows = resbs.select { |a| a.matnr.eql?(row.material) and a.werks.eql?(row.plant) and
-      #           a.arbpl.eql?(row.work_center) }
-      #       if rows.present?
-      #         resb = rows.first
-      #         resb.rspos = MesTErpAccount.create_sap_resb(resb.rsnum, resb.rspos)
-      #       end
-      #     end
-      #
-      #     if resb.present?
-      #       key = "#{row.plant}.#{row.material}.#{row.bacth}"
-      #       if mchbs.key?(key)
-      #         mchbs[key].each do |mchb|
-      #           if mchb.clabs > 0
-      #             resb_qty = row.ws_bal_qty > mchb.clabs ? mchb.clabs : row.ws_bal_qty
-      #             row.ws_bal_qty -= resb_qty
-      #             row.ws_alloc_qty += resb_qty
-      #             mchb.clabs -= resb_qty
-      #             resb.bal_qty -= resb_qty
-      #             row.ws_movements.append({
-      #                                         rsnum: resb.rsnum, rspos: resb.rspos, rsart: resb.rsart,
-      #                                         lgort: mchb.lgort, menge: resb_qty
-      #                                     })
-      #           end
-      #           break if row.ws_bal_qty == 0
-      #         end #mchbs[key].each do |mchb|
-      #       end
-      #     end
-      #   end #if row.ws_bal_qty > 0
-      # end #mes_t_erp_accounts.each do |row|
-
+            end # mes_t_erp_accounts.each do |row|
+          end #if resb.bal_qty > 0
+        end # resbs.each do |resb|
+      end
       bapi_goodsmvt_create_261(mes_t_erp_accounts)
-      #return mes_t_erp_accounts
-      # mes_t_erp_accounts.each do |row|
-      #   row.ws_movements.each do |hash|
-      #     puts "#{row.material} |#{row.ws_bal_qty}| #{row.quantity} #{hash}"
-      #   end
-      # end
     end # MesTErpAccount.find_by_sql(sql).each do |order|
-
   end
 
 
@@ -213,7 +239,7 @@ class MesTErpAccount < ActiveRecord::Base
       header.setValue('PSTNG_DATE', Date.today.strftime('%Y%m%d'))
       header.setValue('DOC_DATE', Date.today.strftime('%Y%m%d'))
       header.setValue('PR_UNAME', 'LUM.LIN')
-      header.setValue('HEADER_TXT', mes_t_erp_accounts.first.order_id)
+      header.setValue('HEADER_TXT', 'MES MO ISSUE')
 
       lines = function.getTableParameterList().getTable('GOODSMVT_ITEM')
       line_id = 0
@@ -227,8 +253,10 @@ class MesTErpAccount < ActiveRecord::Base
           lines.setValue('BATCH', resb.bacth)
           lines.setValue('STGE_LOC', hash[:lgort])
           lines.setValue('MOVE_TYPE', '261')
-          #lines.setValue('XSTOB', 'X') #if 262
-          lines.setValue('ORDERID', resb.order_id)
+          if resb.move_type.eql?('262')
+            lines.setValue('XSTOB', 'X')
+          end
+          lines.setValue('ORDERID', hash[:aufnr])
           lines.setValue('RESERV_NO', hash[:rsnum])
           lines.setValue('RES_ITEM', hash[:rspos])
           lines.setValue('RES_TYPE', hash[:rsart])
@@ -257,6 +285,7 @@ class MesTErpAccount < ActiveRecord::Base
           mes_t_erp_accounts.each do |account|
             account.ws_movements.each do |hash|
               line_id += 10
+              menge =  account.move_type.eql?('262') ? hash[:menge] * -1 : hash[:menge]
               MesTErpAccountIss.create(
                   uuid: UUID.new.generate(:compact),
                   tea_id: account.id,
@@ -265,15 +294,17 @@ class MesTErpAccount < ActiveRecord::Base
                   zeile: line_id,
                   matnr: account.material,
                   charg: account.bacth,
-                  menge: hash[:menge],
+                  menge: menge,
                   werks: account.plant,
                   lgort: hash[:lgort],
                   rsnum: hash[:rsnum],
                   rspos: hash[:rspos],
+                  aufnr: hash[:aufnr],
+                  posnr: hash[:posnr],
                   vtweg: PoReceipt.vtweg(account.plant)
               )
-              account.sap_posted_qty += account.ws_alloc_qty
-              account.status = 'X' if account.sap_posted_qty == account.quantity
+              account.sap_posted_qty += menge
+              account.status = 'X' if (account.sap_posted_qty + account.mes_inter_qty) == account.quantity
               account.save
             end
           end
@@ -287,8 +318,18 @@ class MesTErpAccount < ActiveRecord::Base
       commit.execute(dest)
       com.sap.conn.jco.JCoContext.end(dest)
     rescue Exception => exception
+      Mail.defaults do
+        delivery_method :smtp, address: '172.91.1.253', port: 25
+      end
       message = "#{message} #{exception.message} #{exception.backtrace.join('\n')}"
-      puts message
+
+      Mail.deliver do
+        from 'lum.cl@l-e-i.com'
+        to 'lum.cl@l-e-i.com, ted.meng@l-e-i.com'
+        subject 'mes_t_erp_out_project bapi_goodsmvt_create_261'
+        body message
+      end
+
     end
   end
 
